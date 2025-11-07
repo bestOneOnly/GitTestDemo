d506689ac020eedd0839c0e1a94d59f609f5d00f
bestOneOnly <1418286476@qq.com>
2025-11-07 11:28:42 +0800

    第一次提交

---
+++ src/test/java/com/sww/gitdemo/GitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sww.gitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
